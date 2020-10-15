4cf62aa541dc822faf0da87352ddb1bb292755d0
Charlie <55356228+sudo-Chop@users.noreply.github.com>
2020-10-15 17:58:00 -0400

    Add original .ps1 scripts

---
+++ disable-nagles-algorithm.ps1
@@ -0,0 +1,16 @@
+﻿#These values will need to be updated
+$HKLM_REG = 'hklm:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces'
+$NIC = '{YOUR NIC ID}'
+
+$FULL_PATH = Join-Path -Path $HKLM_REG -ChildPath $NIC
+$NIC_INFO = Get-Childitem -path $HKLM_REG -recurse -ErrorAction SilentlyContinue | Where-Object {$_.Name -match $NIC}
+
+#Creates the DWORD Registry Keys nessary to disable algorithm
+Set-Itemproperty -path $FULL_PATH -Name 'TcpAckFrequency' -value '1' -Type "Dword"    
+Set-Itemproperty -path $FULL_PATH -Name 'TCPNoDelay'-value '1' -Type "Dword"
+
+#Prints NIC Registry info to verify changes   
+Write-Host ">>>> Nagles Algorithm Disable <<<<" -ForegroundColor Green
+Write-Host "TcpAckFrequency:'1'" -ForegroundColor Green
+Write-Host "TCPNoDelay:'1'" -ForegroundColor Green
+$NIC_INFO
---
+++ enable-nagles-algorithm.ps1
@@ -0,0 +1,16 @@
+﻿#These values will need to be updated
+$HKLM_REG = 'hklm:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces'
+$NIC = '{YOUR NIC ID}'
+
+$FULL_PATH = Join-Path -Path $HKLM_REG -ChildPath $NIC
+$NIC_INFO = Get-Childitem -path $HKLM_REG -recurse -ErrorAction SilentlyContinue | Where-Object {$_.Name -match $NIC}
+
+#Creates the DWORD Registry Keys nessary to disable algorithm
+Set-Itemproperty -path $FULL_PATH -Name 'TcpAckFrequency' -value '0'    
+Set-Itemproperty -path $FULL_PATH -Name 'TCPNoDelay'-value '0'
+
+#Prints NIC Registry info to verify changes   
+Write-Host ">>>> Nagles Algorithm Enabled <<<<" -ForegroundColor Red
+Write-Host "TcpAckFrequency:'0'" -ForegroundColor Red
+Write-Host "TCPNoDelay:'0'" -ForegroundColor Red
+$NIC_INFO
